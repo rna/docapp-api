c7c9c2aa0dbecf686a0835e711ce36fadd92b07f
Ramesh <rameshnaidu.allu@gmail.com>
2020-11-16 21:11:07 +0530

    scaffold appointments and fix routes for versioning

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :patients
       resources :doctors
+      resources :appointments
     end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
