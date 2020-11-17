45117c0301d774df54976d3f223e72b76e799fb2
Ramesh <rameshnaidu.allu@gmail.com>
2020-11-18 00:50:00 +0530

    add time slots method to  schedule model

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -1,3 +1,14 @@
 class Schedule < ApplicationRecord
   belongs_to :doctor
+
+  def self.time_slots
+    time_slots = []
+
+    Schedule.all.each do |s|
+      (s.start_time.to_i..s.end_time.to_i).step(1800).each do |time|
+        time_slots << Time.at(time)
+      end
+    end
+    time_slots
+  end
 end
