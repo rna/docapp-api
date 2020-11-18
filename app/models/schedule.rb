d1dc58aacfb0d935b95907f26d8d410c08e4bd07
Ramesh <rameshnaidu.allu@gmail.com>
2020-11-18 23:00:34 +0530

    fix default time zone change

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -6,9 +6,10 @@ class Schedule < ApplicationRecord
 
     Schedule.all.each do |s|
       (s.start_time.to_i..s.end_time.to_i).step(s.duration*60).each do |time|
-        time_slots << Time.at(time)
+        time_slots << Time.at(time).utc.strftime("%H:%M")
       end
     end
     time_slots
   end
 end
+
