a3aa90d26182151bdcc6394047461849c06998c7
Ramesh <rameshnaidu.allu@gmail.com>
2020-11-20 20:23:24 +0530

    make timeslots method return an object with id

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -2,13 +2,15 @@ class Schedule < ApplicationRecord
   belongs_to :doctor
 
   def self.time_slots
-    time_slots = []
+    time_slots = {}
 
     Schedule.all.each do |s|
+      time_slots[s.doctor_id] = [] unless time_slots[s.doctor_id]
       (Time.parse(s.start_time).to_i..Time.parse(s.end_time).to_i).step(s.duration * 60).each do |time|
-        time_slots << Time.at(time).strftime('%D %H:%M')
+        slot = Time.at(time).strftime('%D %H:%M')
+        time_slots[s.doctor_id] << slot unless time_slots[s.doctor_id].include?(slot)
       end
     end
-    time_slots.uniq
+    time_slots
   end
 end
