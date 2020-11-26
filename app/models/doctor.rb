165444f94413661b09ce2518c37200aeb2341d1c
Ramesh <rameshnaidu.allu@gmail.com>
2020-11-26 17:28:06 +0530

    add validations for user models

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -3,4 +3,9 @@ class Doctor < ApplicationRecord
 
   has_many :schedules
   has_many :appointments
+
+  VALID_EMAIL_ADDRESS = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
+  validates :email, presence: true, uniqueness: true,
+                    format: { with: VALID_EMAIL_ADDRESS },
+                    length: { minimum: 6, maximum: 100 }
 end
