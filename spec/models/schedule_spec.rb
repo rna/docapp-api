92f32157874f02b91d9247a80b81bf75bd4a7afc
Ramesh <rameshnaidu.allu@gmail.com>
2020-11-30 02:50:58 +0530

    fix linter errors

--- spec/models/schedule_spec.rb
+++ spec/models/schedule_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Schedule, type: :model do
-    describe 'associations' do
+  describe 'associations' do
     it { should have_many(:appointments) }
     it { should belong_to(:doctor) }
   end
