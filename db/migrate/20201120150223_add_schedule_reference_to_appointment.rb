b07b3c4cdefa33b08d06835f6c3ac60c9f1f943a
Ramesh <rameshnaidu.allu@gmail.com>
2020-11-30 12:15:17 +0530

    fix db migration DuplicateColumn error

--- db/migrate/20201120150223_add_schedule_reference_to_appointment.rb
+++ db/migrate/20201120150223_add_schedule_reference_to_appointment.rb
@@ -1,6 +1,5 @@
 class AddScheduleReferenceToAppointment < ActiveRecord::Migration[6.0]
   def change
-    add_reference :appointments, :schedule, foreign_key: true
     remove_column :appointments, :appointment_time
     remove_column :appointments, :date
   end
